1466a19313aefa3b646c6b78dfe255f54a6f648f
Kassim <kassimrooney@gmail.com>
2026-05-21 15:49:23 +0300

    cook Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,36 @@ import "../css/Home.css";
 
 function Home() {
   return (
-    <div className="home">
-      <h1>Welcome to Aurora Spa</h1>
-      <p>Where relaxation begins</p>
-    </div>
+    <section className="hero">
+      <div className="hero-overlay">
+        <div className="hero-content">
+          <span className="hero-tag">
+            Welcome to Aurora Spa
+          </span>
+
+          <h1>
+            Relax, Refresh <br />
+            and Rejuvenate
+          </h1>
+
+          <p>
+            Experience premium massage therapy,
+            luxurious spa treatments and total
+            wellness in a calming environment.
+          </p>
+
+          <div className="hero-buttons">
+            <button className="book-now">
+              Book Appointment
+            </button>
+
+            <button className="explore-btn">
+              Explore Services
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
