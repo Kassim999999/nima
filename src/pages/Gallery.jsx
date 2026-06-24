b552383b35b9703286ccb431fd159f14868601f0
Kassim <kassimrooney@gmail.com>
2026-06-24 11:20:47 +0300

    Rearrangements

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,15 +1,21 @@
 import "../css/Gallery.css";
+import Nima1 from "../assets/Nima1.png";
+import Nima2 from "../assets/Nima2.png";
+import Nima3 from "../assets/Nima3.png";
+import Nima4 from "../assets/Nima4.png";
+import Nima6 from "../assets/Nima6.png";
+import Nima11 from "../assets/Nima11.png";
 
 function Gallery() {
 
-  const images = [
-     "/src/assets/Nima1.png",
-  "/src/assets/Nima2.png",
-  "/src/assets/Nima3.png",
-  "/src/assets/Nima4.png",
-  "/src/assets/Nima6.png",
-  "/src/assets/Nima11.png",
-  ];
+const images = [
+  Nima1,
+  Nima2,
+  Nima3,
+  Nima4,
+  Nima6,
+  Nima11,
+];
 
   return (
     <div className="gallery-page">
